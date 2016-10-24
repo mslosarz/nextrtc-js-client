94832a53730ecf956b75cc2b9117185be89b98b7
Marcin Ślósarz <m.slosarz@ahej.pl>
2016-10-24 23:16:17 +0200

    added mechanism to collect request until websocket channel will be ready to transfer data

--- nextRTC.js
+++ nextRTC.js
@@ -13,6 +13,8 @@ var NextRTC = function NextRTC(config) {
     this.peerConnections = {};
     this.localStream = null;
     this.signals = {};
+    this.channelReady = false;
+    this.waiting = [];
 
     this.call = function(event, data) {
         for ( var signal in this.signals) {
@@ -29,14 +31,23 @@ var NextRTC = function NextRTC(config) {
             to: to,
             content: convId,
             custom: custom });
-        console.log("res: " + req);
-        this.signaling.send(req);
+        if(!this.channelReady){
+            this.waiting.push(req);
+        } else {
+            console.log("req: " + req);
+            this.signaling.send(req);
+        }
     };
 
     this.signaling.onmessage = function(event) {
-        console.log("req: " + event.data);
+        console.log("res: " + event.data);
         var signal = JSON.parse(event.data);
         that.call(signal.signal, signal);
+    };
+
+    this.signaling.onopen = function() {
+        console.log("channel ready");
+        that.setChannelReady();
     };
 
     this.signaling.onclose = function(event) {
@@ -46,6 +57,14 @@ var NextRTC = function NextRTC(config) {
     this.signaling.onerror = function(event) {
         that.call('error', event);
     };
+
+    this.setChannelReady = function(){
+        for(var w in that.waiting){
+            console.log("req: " + w);
+            that.signaling.send(w);
+        }
+        that.channelReady = true;
+    }
 
     this.preparePeerConnection = function(nextRTC, member) {
         if (nextRTC.peerConnections[member] == undefined) {
